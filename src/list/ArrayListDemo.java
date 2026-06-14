0dd8b97bf4d908224237433c25cc9ee0564f66e7
Vivekananda <vivekanandadash245@gmail.com>
2026-06-14 23:46:10 +0530

    Initial commit for colections

--- src/list/ArrayListDemo.java
+++ src/list/ArrayListDemo.java
@@ -1,4 +1,19 @@
 package list;
 
+import java.util.ArrayList;
+
 public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<Object> al = new ArrayList<>();
+        al.add(24);
+        al.add(200);
+        al.add("Rama");
+        al.add("Hari");
+        al.add(25.02);
+        System.out.println(al);
+        System.out.println("Size of the Collections ::" + al.size());
+        al.remove(0);
+        System.out.println("Size of the Collections ::" + al.size());
+        System.out.println(al);
+    }
 }
