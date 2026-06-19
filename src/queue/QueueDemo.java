fdab9f39bd59217c570ed9f4afc43b0c607e0fe0
Vivekananda <vivekanandadash245@gmail.com>
2026-06-20 00:00:39 +0530

    Practice Queue In collection

--- src/queue/QueueDemo.java
+++ src/queue/QueueDemo.java
@@ -1,4 +1,32 @@
 package queue;
 
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
 public class QueueDemo {
+    public static void main(String[] args) {
+        PriorityQueue<String> queue = new PriorityQueue<>();
+        queue.add("john");
+        queue.add("smith");
+        queue.add("orlen");
+        queue.add("charles");
+
+        System.out.println(queue);
+
+        System.out.println(queue.element());
+        System.out.println(queue.peek());
+        System.out.println("=====================================");
+        Iterator<String> iterator = queue.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+        queue.remove(); // retrieve and remove the head of the queue
+        queue.poll();  // retrieve and remove the head of the queue
+        System.out.println(queue);
+
+
+
+
+    }
 }
