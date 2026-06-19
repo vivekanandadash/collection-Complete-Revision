e9de684afe5577f87755a09cc4279e9c0698dbd9
Vivekananda <vivekanandadash245@gmail.com>
2026-06-19 23:59:56 +0530

    Pracctice Treemap

--- src/map/TreeMapDemo.java
+++ src/map/TreeMapDemo.java
@@ -1,4 +1,17 @@
 package map;
 
+import java.util.TreeMap;
+
 public class TreeMapDemo {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> map = new TreeMap<>();
+        map.put("zebra" ,108);
+        map.put("yabura",3);
+        map.put("libana",45);
+        map.put("alice", 45);
+
+        System.out.println(map);
+
+
+    }
 }
