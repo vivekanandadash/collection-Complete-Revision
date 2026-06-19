452ae4f50c21fcf7d9c10e155a62349136fd1fe0
Vivekananda <vivekanandadash245@gmail.com>
2026-06-20 00:01:48 +0530

    Practice ArrayDequeue Questions

--- src/queue/ArrayDequeueDemo.java
+++ src/queue/ArrayDequeueDemo.java
@@ -1,4 +1,21 @@
 package queue;
 
+import java.util.ArrayDeque;
+
 public class ArrayDequeueDemo {
+    public static void main(String[] args) {
+        ArrayDeque<String> ad = new ArrayDeque<>();
+        ad.add("one");
+        ad.add("two");
+        ad.add("three");
+        ad.addFirst("vivek");
+        ad.addLast("gudulu");
+        System.out.println(ad);
+
+        ad.pollFirst();
+
+        System.out.println(ad);
+        ad.pollLast();
+        System.out.println(ad);
+    }
 }
