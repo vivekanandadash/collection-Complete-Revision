d1aa4b3ba278357c61066e8e4ec2d33ba8b44328
Vivekananda <vivekanandadash245@gmail.com>
2026-06-19 23:59:05 +0530

    Practice linkedHAshmap questions and implementation

--- src/map/LinkedHashmapDemo.java
+++ src/map/LinkedHashmapDemo.java
@@ -1,4 +1,17 @@
 package map;
 
+import java.util.LinkedHashMap;
+
 public class LinkedHashmapDemo {
+    public static void main(String[] args) {
+        LinkedHashMap<Integer,String> color = new LinkedHashMap<>();
+
+        color.put(104 ,"Blue");
+        color.put(125 ,"Yellow");
+        color.put(45 ,"Green");
+        color.put(78 ,"White");
+
+        System.out.println(color);
+
+    }
 }
