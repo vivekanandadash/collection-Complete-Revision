778d4792dbcceadb476071eb792421aa4c6af1c6
Vivekananda <vivekanandadash245@gmail.com>
2026-06-21 23:54:05 +0530

    add studentDemo for Sorting the collection using Collection.sort method where this method need that Object must implement Comparable interface and overide its abstract method

--- src/sorting/StudentDemo.java
+++ src/sorting/StudentDemo.java
@@ -1,4 +1,19 @@
 package sorting;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class StudentDemo {
+    public static void main(String[] args) {
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(new Student(108,"Alice",2));
+        studentList.add(new Student(104,"Bob",4));
+        studentList.add(new Student(109,"Charlie",3));
+        studentList.add(new Student(101,"David",1));
+        studentList.add(new Student(105,"Evance",7));
+
+        Collections.sort(studentList);
+        studentList.forEach(System.out::println);
+    }
 }
