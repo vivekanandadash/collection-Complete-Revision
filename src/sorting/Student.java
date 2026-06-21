f5155fcd658cbe1ee1f2c3ac1b6d12f7ecde95b5
Vivekananda <vivekanandadash245@gmail.com>
2026-06-21 23:45:55 +0530

    Add custom object sorting using Comparable interface

--- src/sorting/Student.java
+++ src/sorting/Student.java
@@ -1,4 +1,26 @@
 package sorting;
 
-public class Student {
+public class Student implements Comparable<Student> {
+    int id;
+    String name;
+    int rank;
+
+    public Student(int id , String name , int rank){
+        this.id = id;
+        this.name = name;
+        this.rank = rank;
+    }
+    @Override
+    public int compareTo(Student s) {
+        return this.id - s.id;
+    }
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", rank=" + rank +
+                '}';
+    }
+
 }
