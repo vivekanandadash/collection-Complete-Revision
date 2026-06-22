5c68083e30ee5880b787786a0b4c96fbad6de19e
Vivekananda <vivekanandadash245@gmail.com>
2026-06-22 16:10:11 +0530

    As Comparable method compare one method at a time so for evey every field wise comparision  we need to change the Student Class

--- src/sorting/Student.java
+++ src/sorting/Student.java
@@ -12,7 +12,9 @@ public class Student implements Comparable<Student> {
     }
     @Override
     public int compareTo(Student s) {
-        return this.id - s.id;
+//        return this.id - s.id;   // Sorting on the basis of id
+//        return this.name.compareTo(s.name); // Sorting on the basis of name as name is String , which is a predefined class which implements comparable so directly use compareTo method with this
+        return this.rank - s.rank; // Sorting as per rank of the object
     }
     @Override
     public String toString() {
--- src/sorting/StudentDemo.java
+++ src/sorting/StudentDemo.java
@@ -8,10 +8,10 @@ public class StudentDemo {
     public static void main(String[] args) {
         List<Student> studentList = new ArrayList<>();
         studentList.add(new Student(108,"Alice",2));
+        studentList.add(new Student(105,"Evance",7));
         studentList.add(new Student(104,"Bob",4));
         studentList.add(new Student(109,"Charlie",3));
         studentList.add(new Student(101,"David",1));
-        studentList.add(new Student(105,"Evance",7));
 
         Collections.sort(studentList);
         studentList.forEach(System.out::println);
