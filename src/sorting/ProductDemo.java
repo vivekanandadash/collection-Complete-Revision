c0152320382557d91845c587b262d9e9ce88a09a
Vivekananda <vivekanandadash245@gmail.com>
2026-06-23 23:42:09 +0530

    Adding Implementation Main method class

--- src/sorting/ProductDemo.java
+++ src/sorting/ProductDemo.java
@@ -1,4 +1,19 @@
 package sorting;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductDemo {
+    public static void main(String[] args) {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product(201, "Jeans pant" , 3999.00 , 5000) );
+        products.add(new Product(202, "Cargo pant" , 1842 , 2451) );
+        products.add(new Product(203, "Korean pant" , 2541 , 1458) );
+        products.add(new Product(204, "Half pant" , 1052 , 9856) );
+
+        products.sort(new ProductIdComparator());
+        for (Product product : products){
+            System.out.println(product);
+        }
+    }
 }
