93bc315a76a88ae53b24283a3ed9c4fa1a00c2c5
Vivekananda <vivekanandadash245@gmail.com>
2026-06-23 23:39:26 +0530

    Adding According to name comparator

--- src/sorting/EmployeeNameComparator.java
+++ src/sorting/EmployeeNameComparator.java
@@ -1,4 +1,10 @@
 package sorting;
 
-public class EmployeeNameComparator {
+import java.util.Comparator;
+
+public class EmployeeNameComparator implements Comparator<Employee> {
+    @Override
+    public int compare(Employee e1, Employee e2) {
+        return e1.name.compareTo(e2.name);
+    }
 }
