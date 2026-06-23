0bf304c4ae7f3441daa4a04436bb02f48b4fa625
Vivekananda <vivekanandadash245@gmail.com>
2026-06-23 23:38:50 +0530

    Add Comparator class for Sorting as per id

--- src/sorting/EmployeeIdComparator.java
+++ src/sorting/EmployeeIdComparator.java
@@ -1,4 +1,11 @@
 package sorting;
 
-public class EmployeeIdComparator {
+import java.util.Comparator;
+
+public class EmployeeIdComparator implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee e1, Employee e2) {
+        return e1.id - e2.id;
+    }
 }
