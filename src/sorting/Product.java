05d2893229e9c59adf06e5d39411ff9809ee235a
Vivekananda <vivekanandadash245@gmail.com>
2026-06-23 23:41:30 +0530

    adding Product Class

--- src/sorting/Product.java
+++ src/sorting/Product.java
@@ -1,4 +1,25 @@
 package sorting;
 
 public class Product {
+    int id ;
+    String name;
+    double price;
+    long eks;
+
+    public Product(int id , String name, double price , long eks){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.eks = eks ;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", eks=" + eks +
+                '}';
+    }
 }
