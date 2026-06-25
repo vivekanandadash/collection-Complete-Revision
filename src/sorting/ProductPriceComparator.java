9b6162808687852a319fbca049f991b3fa9ca5ed
Vivekananda <vivekanandadash245@gmail.com>
2026-06-26 00:02:09 +0530

    Adding product price comprator

--- src/sorting/ProductPriceComparator.java
+++ src/sorting/ProductPriceComparator.java
@@ -1,5 +1,12 @@
 package sorting;
 
-public class ProductPriceComparator {
+import java.util.Comparator;
 
+public class ProductPriceComparator implements Comparator<Product> {
+
+
+    @Override
+    public int compare(Product p1, Product p2) {
+        return Double.compare(p1.price, p2.price);
+    }
 }
