53cde0b5662f53a0f456e4276775a9fac6faa30b
Vivekananda <vivekanandadash245@gmail.com>
2026-06-26 00:08:03 +0530

    Adding Sorting

--- src/sorting/ProductDemo.java
+++ src/sorting/ProductDemo.java
@@ -10,8 +10,11 @@ public class ProductDemo {
         products.add(new Product(202, "Cargo pant" , 1842 , 2451) );
         products.add(new Product(203, "Korean pant" , 2541 , 1458) );
         products.add(new Product(204, "Half pant" , 1052 , 9856) );
+//        Comparing and sorting based on id
+//        products.sort(new ProductIdComparator());
 
-        products.sort(new ProductIdComparator());
+//        Comparing and sorting based on price
+        products.sort(new ProductPriceComparator());
         for (Product product : products){
             //Print the number
             System.out.println(product);
