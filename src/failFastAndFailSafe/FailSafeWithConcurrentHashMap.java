cd15d89fdf2de084554b4fb4f1251f9427449a20
Vivekananda <vivekanandadash245@gmail.com>
2026-06-26 23:50:48 +0530

    Adding Failsafe with concurrentHashMap

--- src/failFastAndFailSafe/FailSafeWithConcurrentHashMap.java
+++ src/failFastAndFailSafe/FailSafeWithConcurrentHashMap.java
@@ -1,4 +1,23 @@
 package failFastAndFailSafe;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class FailSafeWithConcurrentHashMap {
+    public static void main(String[] args) {
+        ConcurrentHashMap<Integer ,String> map = new ConcurrentHashMap<>();
+        map.put(1,"Alice");
+        map.put(2,"Bob");
+        map.put(3,"Charlie");
+
+        ConcurrentHashMap.KeySetView<Integer, String> integers = map.keySet();
+        Iterator<Integer> iterator = integers.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+            map.put(4 , "David");
+        }
+
+        System.out.println(map);
+    }
 }
