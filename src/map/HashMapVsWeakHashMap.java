679eecda135b296212edfff37fdfa7a19945c5af
Vivekananda <vivekanandadash245@gmail.com>
2026-06-26 23:52:49 +0530

    Adding HashMAp vs WeakHAshMAp

--- src/map/HashMapVsWeakHashMap.java
+++ src/map/HashMapVsWeakHashMap.java
@@ -1,4 +1,16 @@
 package map;
 
+import java.util.WeakHashMap;
+
 public class HashMapVsWeakHashMap {
+    public static void main(String[] args) {
+        WeakHashMap<Integer,String> whm = new WeakHashMap<>();
+        whm.put(1,"Alice");
+        whm.put(2,"Bob");
+        whm.put(3,"charlie");
+        System.out.println(whm);
+
+
+
+    }
 }
