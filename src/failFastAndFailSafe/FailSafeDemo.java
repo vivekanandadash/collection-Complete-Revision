daac3878ca6b92585cbbd04aa2e69a9ceb7b9aea
Vivekananda <vivekanandadash245@gmail.com>
2026-06-26 23:49:29 +0530

    adding failsafe

--- src/failFastAndFailSafe/FailSafeDemo.java
+++ src/failFastAndFailSafe/FailSafeDemo.java
@@ -1,4 +1,20 @@
 package failFastAndFailSafe;
 
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class FailSafeDemo {
+    public static void main(String[] args) {
+        CopyOnWriteArrayList<Integer> al = new CopyOnWriteArrayList<>();
+        al.add(100);
+        al.add(200);
+        al.add(300);
+        al.add(400);
+
+        for (Integer num : al){
+            if (num == 200){
+                al.add(500);
+            }
+        }
+        System.out.println(al);
+    }
 }
