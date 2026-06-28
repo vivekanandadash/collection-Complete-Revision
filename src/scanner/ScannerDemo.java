0943c6098715d2505ad63ab04e8c743c2978f31d
Vivekananda <vivekanandadash245@gmail.com>
2026-06-28 13:34:16 +0530

    Adding String tokenizer

--- src/scanner/ScannerDemo.java
+++ src/scanner/ScannerDemo.java
@@ -1,4 +1,15 @@
 package scanner;
 
+import java.util.Scanner;
+
 public class ScannerDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int i = sc.nextInt();
+        System.out.println("Enter the second number");
+        int j = sc.nextInt();
+        System.out.println("The sum of both is :" + (i+j));
+        sc.close();
+    }
 }
--- src/stringTokenizer/StringTokenizerDemo.java
+++ src/stringTokenizer/StringTokenizerDemo.java
@@ -1,4 +1,13 @@
 package stringTokenizer;
 
+import java.util.StringTokenizer;
+
 public class StringTokenizerDemo {
+    public static void main(String[] args) {
+        String s = "Java Programming Class";
+        StringTokenizer st = new StringTokenizer(s , " ");
+        while (st.hasMoreTokens()){
+            System.out.println(st.nextToken());
+        }
+    }
 }
